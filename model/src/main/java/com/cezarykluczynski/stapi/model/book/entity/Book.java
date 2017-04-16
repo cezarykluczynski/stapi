94b177220d7e70d9d732e972f037d782f622a343
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-04-16 19:50:29 +0200

    Book model added

---
+++ model/src/main/java/com/cezarykluczynski/stapi/model/book/entity/Book.java
@@ -0,0 +1,155 @@
+package com.cezarykluczynski.stapi.model.book.entity;
+
+import com.cezarykluczynski.stapi.model.character.entity.Character;
+import com.cezarykluczynski.stapi.model.common.entity.PageAwareEntity;
+import com.cezarykluczynski.stapi.model.company.entity.Company;
+import com.cezarykluczynski.stapi.model.page.entity.PageAware;
+import com.cezarykluczynski.stapi.model.reference.entity.Reference;
+import com.cezarykluczynski.stapi.model.staff.entity.Staff;
+import com.google.common.collect.Sets;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true, exclude = {"authors", "artists", "editors", "audiobookNarrators", "publishers", "audiobookPublishers", "characters",
+		"references", "audiobookReferences"})
+@EqualsAndHashCode(callSuper = true, exclude = {"authors", "artists", "editors", "audiobookNarrators", "publishers", "audiobookPublishers",
+		"characters", "references", "audiobookReferences"})
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Book extends PageAwareEntity implements PageAware {
+
+	@Id
+	@Column(nullable = false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_sequence_generator")
+	@SequenceGenerator(name = "book_sequence_generator", sequenceName = "book_sequence", allocationSize = 1)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	private Integer publishedYear;
+
+	private Integer publishedMonth;
+
+	private Integer publishedDay;
+
+	private Integer numberOfPages;
+
+	private Float stardateFrom;
+
+	private Float stardateTo;
+
+	private Integer yearFrom;
+
+	private Integer yearTo;
+
+	private Boolean novel;
+
+	private Boolean referenceBook;
+
+	private Boolean rolePlayingBook;
+
+	private Boolean biographyBook;
+
+	@SuppressWarnings("MemberName")
+	private Boolean eBook;
+
+	private Boolean anthology;
+
+	private Boolean novelization;
+
+	private Boolean audiobook;
+
+	private Boolean audiobookAbridged;
+
+	private Integer audiobookPublishedYear;
+
+	private Integer audiobookPublishedMonth;
+
+	private Integer audiobookPublishedDay;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "books_authors",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "staff_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Staff> authors = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "books_artists",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "staff_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Staff> artists = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "books_editors",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "staff_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Staff> editors = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "audiobooks_narrators",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "staff_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Staff> audiobookNarrators = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "book_publishers",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "company_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Company> publishers = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "audiobooks_publishers",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "company_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Company> audiobookPublishers = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "books_characters",
+			joinColumns = @JoinColumn(name = "comics_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "character_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Character> characters = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "books_references",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "reference_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Reference> references = Sets.newHashSet();
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "audiobooks_references",
+			joinColumns = @JoinColumn(name = "book_id", nullable = false, updatable = false),
+			inverseJoinColumns = @JoinColumn(name = "reference_id", nullable = false, updatable = false))
+	@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+	private Set<Reference> audiobookReferences = Sets.newHashSet();
+
+}
---
+++ model/src/main/java/com/cezarykluczynski/stapi/model/book/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.cezarykluczynski.stapi.model.book.repository;
+
+import com.cezarykluczynski.stapi.model.book.entity.Book;
+import com.cezarykluczynski.stapi.model.common.repository.PageAwareRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long>, PageAwareRepository<Book> {
+}
