e865a17b3c2faec26183351bada4df08ef618167
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-04-27 06:37:59 +0200

    Another fix to UIDs

--- etl/src/main/java/com/cezarykluczynski/stapi/etl/configuration/job/SpringBatchDaoConfiguration.java
+++ etl/src/main/java/com/cezarykluczynski/stapi/etl/configuration/job/SpringBatchDaoConfiguration.java
@@ -11,11 +11,13 @@ import org.springframework.batch.core.repository.support.SimpleJobRepository;
 import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import javax.inject.Inject;
 import java.lang.reflect.Field;
 
 @Configuration
+@DependsOn("batchDatabaseInitializer")
 public class SpringBatchDaoConfiguration {
 
 	@Inject
