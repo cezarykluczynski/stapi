a941a223160b182f19f37453b4a3a61507b97e3b
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-05-15 20:16:18 +0200

    Trying to fix Semaphone build

--- server/src/test/groovy/com/cezarykluczynski/stapi/server/common/documentation/service/DocumentationReaderTest.groovy
+++ server/src/test/groovy/com/cezarykluczynski/stapi/server/common/documentation/service/DocumentationReaderTest.groovy
@@ -12,26 +12,23 @@ class DocumentationReaderTest extends Specification {
 		documentationReader = new DocumentationReader()
 	}
 
+	@SuppressWarnings('ClosureAsLastMethodParameter')
 	void "reads directory into list of DocumentDTO, recursively"() {
 		given:
 		String rootDirectory = '../contract/src/main/resources/v1/swagger/book'
-		String contractDirectory = './contract/src/main/resources/v1/swagger/book'
+		String contractDirectory = 'contract/src/main/resources/v1/swagger/book'
 		String directory = new File(rootDirectory).isDirectory() ? rootDirectory : contractDirectory
 
 		when:
 		List<DocumentDTO> documentDTOList = documentationReader.readDirectory(directory)
-
+		DocumentDTO documentDTO = documentationReader.readDirectory(directory).stream()
+					.filter({ it.path.endsWith('bookBase.yaml') })
+					.findFirst().get()
 		then:
 		documentDTOList.size() == 7
-		documentDTOList[0].path.endsWith 'bookBase.yaml'
-		documentDTOList[0].content.contains 'rolePlayingBook:'
-		documentDTOList[0].type == DocumentType.YAML
-		documentDTOList[1].path.endsWith 'bookBaseResponse.yaml'
-		documentDTOList[2].path.endsWith 'bookFull.yaml'
-		documentDTOList[3].path.endsWith 'bookFullResponse.yaml'
-		documentDTOList[4].path.endsWith 'bookHeader.yaml'
-		documentDTOList[5].path.endsWith 'book.path.yaml'
-		documentDTOList[6].path.endsWith 'bookSearch.path.yaml'
+		documentDTO.path.endsWith 'bookBase.yaml'
+		documentDTO.content.contains 'rolePlayingBook:'
+		documentDTO.type == DocumentType.YAML
 	}
 
 }
