ab6ae86a91bb75ae3977ceafcf75dde62dfb20e4
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-05-24 21:06:38 +0200

    EpisodeWriter should filter duplicates too

--- etl/src/main/java/com/cezarykluczynski/stapi/etl/episode/creation/processor/EpisodeWriter.java
+++ etl/src/main/java/com/cezarykluczynski/stapi/etl/episode/creation/processor/EpisodeWriter.java
@@ -2,25 +2,51 @@ package com.cezarykluczynski.stapi.etl.episode.creation.processor;
 
 import com.cezarykluczynski.stapi.model.episode.entity.Episode;
 import com.cezarykluczynski.stapi.model.episode.repository.EpisodeRepository;
+import com.cezarykluczynski.stapi.model.page.entity.PageAware;
+import com.cezarykluczynski.stapi.model.page.service.DuplicateFilteringPreSavePageAwareFilter;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EpisodeWriter implements ItemWriter<Episode> {
 
 	private EpisodeRepository episodeRepository;
 
+	private DuplicateFilteringPreSavePageAwareFilter duplicateFilteringPreSavePageAwareProcessor;
+
 	@Inject
-	public EpisodeWriter(EpisodeRepository episodeRepository) {
+	public EpisodeWriter(EpisodeRepository episodeRepository, DuplicateFilteringPreSavePageAwareFilter duplicateFilteringPreSavePageAwareProcessor) {
 		this.episodeRepository = episodeRepository;
+		this.duplicateFilteringPreSavePageAwareProcessor = duplicateFilteringPreSavePageAwareProcessor;
 	}
 
 	@Override
 	public void write(List<? extends Episode> items) throws Exception {
-		episodeRepository.save(items);
+		episodeRepository.save(process(items));
+	}
+
+	private List<Episode> process(List<? extends Episode> episodeList) {
+		List<Episode> episodeListWithoutExtends = fromExtendsListToEpisodeList(episodeList);
+		return filterDuplicates(episodeListWithoutExtends);
+	}
+
+	private List<Episode> fromExtendsListToEpisodeList(List<? extends Episode> episodeList) {
+		return episodeList
+				.stream()
+				.map(pageAware -> (Episode) pageAware)
+				.collect(Collectors.toList());
+	}
+
+	private List<Episode> filterDuplicates(List<Episode> episodeList) {
+		return duplicateFilteringPreSavePageAwareProcessor.process(episodeList.stream()
+				.map(episode -> (PageAware) episode)
+				.collect(Collectors.toList()), Episode.class).stream()
+				.map(pageAware -> (Episode) pageAware)
+				.collect(Collectors.toList());
 	}
 
 }
--- etl/src/test/groovy/com/cezarykluczynski/stapi/etl/episode/creation/processor/EpisodeWriterTest.groovy
+++ etl/src/test/groovy/com/cezarykluczynski/stapi/etl/episode/creation/processor/EpisodeWriterTest.groovy
@@ -2,6 +2,7 @@ package com.cezarykluczynski.stapi.etl.episode.creation.processor
 
 import com.cezarykluczynski.stapi.model.episode.entity.Episode
 import com.cezarykluczynski.stapi.model.episode.repository.EpisodeRepository
+import com.cezarykluczynski.stapi.model.page.service.DuplicateFilteringPreSavePageAwareFilter
 import com.google.common.collect.Lists
 import spock.lang.Specification
 
@@ -9,22 +10,29 @@ class EpisodeWriterTest extends Specification {
 
 	private EpisodeRepository episodeRepositoryMock
 
-	private EpisodeWriter episodeWriter
+	private DuplicateFilteringPreSavePageAwareFilter duplicateFilteringPreSavePageAwareProcessorMock
+
+	private EpisodeWriter episodeWriterMock
 
 	void setup() {
 		episodeRepositoryMock = Mock()
-		episodeWriter = new EpisodeWriter(episodeRepositoryMock)
+		duplicateFilteringPreSavePageAwareProcessorMock = Mock()
+		episodeWriterMock = new EpisodeWriter(episodeRepositoryMock, duplicateFilteringPreSavePageAwareProcessorMock)
 	}
 
-	void "writes all entities using repository"() {
+	void "filters all entities using pre save processor, then writes all entities using repository"() {
 		given:
 		Episode episode = new Episode()
 		List<Episode> episodeList = Lists.newArrayList(episode)
 
 		when:
-		episodeWriter.write(episodeList)
+		episodeWriterMock.write(episodeList)
 
 		then:
+		1 * duplicateFilteringPreSavePageAwareProcessorMock.process(_, Episode) >> { args ->
+			assert args[0][0] == episode
+			episodeList
+		}
 		1 * episodeRepositoryMock.save(episodeList)
 		0 * _
 	}
