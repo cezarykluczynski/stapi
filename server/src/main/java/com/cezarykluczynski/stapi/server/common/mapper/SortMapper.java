66b976120d10e97e22fa282da77756478042713c
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-06-04 09:58:10 +0200

    SortMapper implemented

--- server/src/main/java/com/cezarykluczynski/stapi/server/common/mapper/SortMapper.java
+++ server/src/main/java/com/cezarykluczynski/stapi/server/common/mapper/SortMapper.java
@@ -1,15 +1,50 @@
 package com.cezarykluczynski.stapi.server.common.mapper;
 
 import com.cezarykluczynski.stapi.client.v1.soap.RequestSort;
+import com.cezarykluczynski.stapi.client.v1.soap.RequestSortClause;
+import com.cezarykluczynski.stapi.client.v1.soap.RequestSortDirectionEnum;
 import com.cezarykluczynski.stapi.client.v1.soap.ResponseSort;
+import com.cezarykluczynski.stapi.client.v1.soap.ResponseSortClause;
+import com.cezarykluczynski.stapi.client.v1.soap.ResponseSortDirectionEnum;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SortMapper {
 
 	public ResponseSort map(RequestSort sort) {
-		// TODO
-		return null;
+		ResponseSort responseSort = new ResponseSort();
+		responseSort.getClauses().addAll(mapClauses(sort.getClauses()));
+		return responseSort;
+	}
+
+	private List<ResponseSortClause> mapClauses(List<RequestSortClause> requestSortClauseList) {
+		return Optional.ofNullable(requestSortClauseList).orElse(Lists.newArrayList()).stream()
+				.map(this::mapClause)
+				.sorted(Comparator.comparing(ResponseSortClause::getClauseOrder))
+				.collect(Collectors.toList());
+	}
+
+	private ResponseSortClause mapClause(RequestSortClause requestSortClause) {
+		ResponseSortClause responseSortClause = new ResponseSortClause();
+		responseSortClause.setName(requestSortClause.getName());
+		responseSortClause.setDirection(mapDirection(requestSortClause.getDirection()));
+		responseSortClause.setClauseOrder(mapClauseOrder(requestSortClause.getClauseOrder()));
+		return responseSortClause;
+	}
+
+	private ResponseSortDirectionEnum mapDirection(RequestSortDirectionEnum requestSortDirectionEnum) {
+		return requestSortDirectionEnum == null ? ResponseSortDirectionEnum.ASC : ResponseSortDirectionEnum.valueOf(requestSortDirectionEnum.name());
+	}
+
+	private Integer mapClauseOrder(Integer clauseOrder) {
+		return ObjectUtils.defaultIfNull(clauseOrder, 0);
 	}
 
 }
---
+++ server/src/test/groovy/com/cezarykluczynski/stapi/server/common/mapper/SortMapperTest.groovy
@@ -0,0 +1,60 @@
+package com.cezarykluczynski.stapi.server.common.mapper
+
+import com.cezarykluczynski.stapi.client.v1.soap.RequestSort
+import com.cezarykluczynski.stapi.client.v1.soap.RequestSortClause
+import com.cezarykluczynski.stapi.client.v1.soap.RequestSortDirectionEnum
+import com.cezarykluczynski.stapi.client.v1.soap.ResponseSort
+import com.cezarykluczynski.stapi.client.v1.soap.ResponseSortDirectionEnum
+import org.assertj.core.util.Lists
+import spock.lang.Specification
+
+class SortMapperTest extends Specification {
+
+	private static final String FIELD_1 = 'FIELD_1'
+	private static final String FIELD_2 = 'FIELD_2'
+	private static final String FIELD_3 = 'FIELD_3'
+	private static final RequestSortDirectionEnum REQUEST_DIRECTION_ASC = RequestSortDirectionEnum.ASC
+	private static final RequestSortDirectionEnum REQUEST_DIRECTION_DESC = RequestSortDirectionEnum.DESC
+	private static final ResponseSortDirectionEnum RESPONSE_DIRECTION_ASC = ResponseSortDirectionEnum.ASC
+	private static final ResponseSortDirectionEnum RESPONSE_DIRECTION_DESC = ResponseSortDirectionEnum.DESC
+
+	private SortMapper sortMapper
+
+	void setup() {
+		sortMapper = new SortMapper()
+	}
+
+	void "maps RequestSort to ResponseSort"() {
+		given:
+		RequestSort requestSort = new RequestSort(clauses: Lists.newArrayList(
+				new RequestSortClause(
+						name: FIELD_3,
+						direction: REQUEST_DIRECTION_ASC,
+						clauseOrder: 2
+				),
+				new RequestSortClause(
+						name: FIELD_1,
+						direction: REQUEST_DIRECTION_DESC,
+				),
+				new RequestSortClause(
+						name: FIELD_2,
+						clauseOrder: 1
+				)
+		))
+
+		when:
+		ResponseSort responseSort = sortMapper.map(requestSort)
+
+		then:
+		responseSort.clauses[0].name == FIELD_1
+		responseSort.clauses[0].direction == RESPONSE_DIRECTION_DESC
+		responseSort.clauses[0].clauseOrder == 0
+		responseSort.clauses[1].name == FIELD_2
+		responseSort.clauses[1].direction == RESPONSE_DIRECTION_ASC
+		responseSort.clauses[1].clauseOrder == 1
+		responseSort.clauses[2].name == FIELD_3
+		responseSort.clauses[2].direction == RESPONSE_DIRECTION_ASC
+		responseSort.clauses[2].clauseOrder == 2
+	}
+
+}
