1008dfa7d0472f1a9629dd427d0a3bad9e5c7be9
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-06-20 06:14:02 +0200

    VideoTemplateParameter added

---
+++ etl/src/main/java/com/cezarykluczynski/stapi/etl/template/video/dto/VideoTemplateParameter.java
@@ -0,0 +1,34 @@
+package com.cezarykluczynski.stapi.etl.template.video.dto;
+
+public class VideoTemplateParameter {
+
+	public static final String FORMAT = "format";
+	public static final String SERIES = "series";
+	public static final String SEASON = "season";
+	public static final String EPISODES = "episodes";
+	public static final String DISCS = "discs";
+	public static final String TIME = "time";
+	public static final String DATE0 = "date0";
+	public static final String DATE = "date";
+	public static final String DATE1S = "date1s";
+	public static final String DATE2 = "date2";
+	public static final String DATE2S = "date2s";
+	public static final String DATE4 = "date4";
+	public static final String DATE4S = "date4s";
+	public static final String RATING = "rating";
+	public static final String LANGUAGE = "language";
+	public static final String SUBTITLES = "subtitles";
+	public static final String DUBBED = "dubbed";
+	public static final String REFERENCE = "reference";
+	public static final String YEAR = "year";
+	public static final String DFAZ = "dfaz";
+	public static final String DFDM = "dfdm";
+	public static final String DFGP = "dfgp";
+	public static final String DFIT = "dfit";
+	public static final String DFUV = "dfuv";
+	public static final String DFVO = "dfvo";
+	public static final String DFVU = "dfvu";
+	public static final String DFXB = "dfxb";
+	public static final String DFYT = "dfyt";
+
+}
