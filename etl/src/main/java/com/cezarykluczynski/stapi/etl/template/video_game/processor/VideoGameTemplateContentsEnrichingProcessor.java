ab1df4d61ca210871d98ed391b21c9002bc8f7e1
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-08-01 18:47:57 +0200

    VideoGameTemplateContentsEnrichingProcessor implemented

--- etl/src/main/java/com/cezarykluczynski/stapi/etl/template/video_game/processor/VideoGameTemplateContentsEnrichingProcessor.java
+++ etl/src/main/java/com/cezarykluczynski/stapi/etl/template/video_game/processor/VideoGameTemplateContentsEnrichingProcessor.java
@@ -2,13 +2,34 @@ package com.cezarykluczynski.stapi.etl.template.video_game.processor;
 
 import com.cezarykluczynski.stapi.etl.common.dto.EnrichablePair;
 import com.cezarykluczynski.stapi.etl.common.processor.ItemEnrichingProcessor;
+import com.cezarykluczynski.stapi.etl.template.common.dto.datetime.StardateRange;
+import com.cezarykluczynski.stapi.etl.template.common.dto.datetime.YearRange;
+import com.cezarykluczynski.stapi.etl.template.common.processor.datetime.DatePartToLocalDateProcessor;
+import com.cezarykluczynski.stapi.etl.template.common.processor.datetime.WikitextToStardateRangeProcessor;
+import com.cezarykluczynski.stapi.etl.template.common.processor.datetime.WikitextToYearRangeProcessor;
 import com.cezarykluczynski.stapi.etl.template.video_game.dto.VideoGameTemplate;
 import com.cezarykluczynski.stapi.etl.template.video_game.dto.VideoGameTemplateParameter;
 import com.cezarykluczynski.stapi.sources.mediawiki.dto.Template;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 class VideoGameTemplateContentsEnrichingProcessor implements ItemEnrichingProcessor<EnrichablePair<Template, VideoGameTemplate>> {
+
+	private final DatePartToLocalDateProcessor datePartToLocalDateProcessor;
+
+	private final WikitextToYearRangeProcessor wikitextToYearRangeProcessor;
+
+	private final WikitextToStardateRangeProcessor wikitextToStardateRangeProcessor;
+
+	public VideoGameTemplateContentsEnrichingProcessor(DatePartToLocalDateProcessor datePartToLocalDateProcessor,
+			WikitextToYearRangeProcessor wikitextToYearRangeProcessor, WikitextToStardateRangeProcessor wikitextToStardateRangeProcessor) {
+		this.datePartToLocalDateProcessor = datePartToLocalDateProcessor;
+		this.wikitextToYearRangeProcessor = wikitextToYearRangeProcessor;
+		this.wikitextToStardateRangeProcessor = wikitextToStardateRangeProcessor;
+	}
 
 	@Override
 	public void enrich(EnrichablePair<Template, VideoGameTemplate> enrichablePair) throws Exception {
@@ -21,12 +42,32 @@ class VideoGameTemplateContentsEnrichingProcessor implements ItemEnrichingProces
 
 			switch (key) {
 				case VideoGameTemplateParameter.TITLE:
+					String originalTitle = videoGameTemplate.getTitle();
+					if (StringUtils.isNotEmpty(value) && !StringUtils.equals(originalTitle, value)) {
+						log.info("Changing video game title \"{}\" (taken from page title) to \"{}\" (taken from template)", originalTitle, value);
+						videoGameTemplate.setTitle(value);
+					}
+					break;
 				case VideoGameTemplateParameter.RELEASED:
+					videoGameTemplate.setReleaseDate(datePartToLocalDateProcessor.process(part));
+					break;
 				case VideoGameTemplateParameter.YEAR:
+					YearRange yearRange = wikitextToYearRangeProcessor.process(value);
+					if (yearRange != null) {
+						videoGameTemplate.setYearFrom(yearRange.getYearFrom());
+						videoGameTemplate.setYearTo(yearRange.getYearTo());
+					}
+					break;
 				case VideoGameTemplateParameter.STARDATE:
+					StardateRange stardateRange = wikitextToStardateRangeProcessor.process(value);
+					if (stardateRange != null) {
+						videoGameTemplate.setStardateFrom(stardateRange.getStardateFrom());
+						videoGameTemplate.setStardateTo(stardateRange.getStardateTo());
+					}
 				case VideoGameTemplateParameter.REQUIREMENTS:
+					videoGameTemplate.setSystemRequirements(value);
+					break;
 				default:
-					// TODO
 					break;
 			}
 		}
---
+++ etl/src/test/groovy/com/cezarykluczynski/stapi/etl/template/video_game/processor/VideoGameTemplateContentsEnrichingProcessorTest.groovy
@@ -0,0 +1,132 @@
+package com.cezarykluczynski.stapi.etl.template.video_game.processor
+
+import com.cezarykluczynski.stapi.etl.common.dto.EnrichablePair
+import com.cezarykluczynski.stapi.etl.template.common.dto.datetime.StardateRange
+import com.cezarykluczynski.stapi.etl.template.common.dto.datetime.YearRange
+import com.cezarykluczynski.stapi.etl.template.common.processor.datetime.DatePartToLocalDateProcessor
+import com.cezarykluczynski.stapi.etl.template.common.processor.datetime.WikitextToStardateRangeProcessor
+import com.cezarykluczynski.stapi.etl.template.common.processor.datetime.WikitextToYearRangeProcessor
+import com.cezarykluczynski.stapi.etl.template.video_game.dto.VideoGameTemplate
+import com.cezarykluczynski.stapi.etl.template.video_game.dto.VideoGameTemplateParameter
+import com.cezarykluczynski.stapi.sources.mediawiki.dto.Template
+import com.cezarykluczynski.stapi.util.AbstractVideoGameTest
+import com.google.common.collect.Lists
+
+class VideoGameTemplateContentsEnrichingProcessorTest extends AbstractVideoGameTest {
+
+	private static final String OLD_TITLE = 'OLD_TITLE'
+	private static final String NEW_TITLE = 'NEW_TITLE'
+	private static final String YEARS = '1995-1997'
+	private static final String STARDATES = '1995-1997'
+
+	private DatePartToLocalDateProcessor datePartToLocalDateProcessorMock
+
+	private WikitextToYearRangeProcessor wikitextToYearRangeProcessorMock
+
+	private WikitextToStardateRangeProcessor wikitextToStardateRangeProcessorMock
+
+	private VideoGameTemplateContentsEnrichingProcessor videoGameTemplateContentsEnrichingProcessor
+
+	void setup() {
+		datePartToLocalDateProcessorMock = Mock()
+		wikitextToYearRangeProcessorMock = Mock()
+		wikitextToStardateRangeProcessorMock = Mock()
+		videoGameTemplateContentsEnrichingProcessor = new VideoGameTemplateContentsEnrichingProcessor(datePartToLocalDateProcessorMock,
+				wikitextToYearRangeProcessorMock, wikitextToStardateRangeProcessorMock)
+	}
+
+	void "sets title from template, when it is different from title already present"() {
+		given:
+		Template.Part templatePart = new Template.Part(key: VideoGameTemplateParameter.TITLE, value: NEW_TITLE)
+		Template template = new Template(parts: Lists.newArrayList(templatePart))
+		VideoGameTemplate videoGameTemplate = new VideoGameTemplate(title: OLD_TITLE)
+		EnrichablePair<Template, VideoGameTemplate> enrichablePair = EnrichablePair.of(template, videoGameTemplate)
+
+		when:
+		videoGameTemplateContentsEnrichingProcessor.enrich(enrichablePair)
+
+		then:
+		0 * _
+		videoGameTemplate.title == NEW_TITLE
+	}
+
+	void "does not sets title from template, when it is equal to title already present"() {
+		given:
+		Template.Part templatePart = new Template.Part(key: VideoGameTemplateParameter.TITLE, value: OLD_TITLE)
+		Template template = new Template(parts: Lists.newArrayList(templatePart))
+		VideoGameTemplate videoGameTemplate = Mock()
+		EnrichablePair<Template, VideoGameTemplate> enrichablePair = EnrichablePair.of(template, videoGameTemplate)
+
+		when:
+		videoGameTemplateContentsEnrichingProcessor.enrich(enrichablePair)
+
+		then:
+		1 * videoGameTemplate.title >> OLD_TITLE
+		0 * _
+	}
+
+	void "sets release date from DatePartToLocalDateProcessor"() {
+		given:
+		Template.Part templatePart = new Template.Part(key: VideoGameTemplateParameter.RELEASED)
+		Template template = new Template(parts: Lists.newArrayList(templatePart))
+		VideoGameTemplate videoGameTemplate = new VideoGameTemplate()
+		EnrichablePair<Template, VideoGameTemplate> enrichablePair = EnrichablePair.of(template, videoGameTemplate)
+
+		when:
+		videoGameTemplateContentsEnrichingProcessor.enrich(enrichablePair)
+
+		then:
+		1 * datePartToLocalDateProcessorMock.process(templatePart) >> RELEASE_DATE
+		videoGameTemplate.releaseDate == RELEASE_DATE
+		0 * _
+	}
+
+	void "sets year from and year to from WikitextToYearRangeProcessor"() {
+		given:
+		Template.Part templatePart = new Template.Part(key: VideoGameTemplateParameter.YEAR, value: YEARS)
+		Template template = new Template(parts: Lists.newArrayList(templatePart))
+		VideoGameTemplate videoGameTemplate = new VideoGameTemplate()
+		YearRange yearRange = new YearRange(yearFrom: YEAR_FROM, yearTo: YEAR_TO)
+
+		when:
+		videoGameTemplateContentsEnrichingProcessor.enrich(EnrichablePair.of(template, videoGameTemplate))
+
+		then:
+		1 * wikitextToYearRangeProcessorMock.process(YEARS) >> yearRange
+		0 * _
+		videoGameTemplate.yearFrom == YEAR_FROM
+		videoGameTemplate.yearTo == YEAR_TO
+	}
+
+	void "sets stardate from and stardate to from WikitextToStardateRangeProcessor"() {
+		given:
+		Template.Part templatePart = new Template.Part(key: VideoGameTemplateParameter.STARDATE, value: STARDATES)
+		Template template = new Template(parts: Lists.newArrayList(templatePart))
+		VideoGameTemplate videoGameTemplate = new VideoGameTemplate()
+		StardateRange stardateRange = new StardateRange(stardateFrom: STARDATE_FROM, stardateTo: STARDATE_TO)
+
+		when:
+		videoGameTemplateContentsEnrichingProcessor.enrich(EnrichablePair.of(template, videoGameTemplate))
+
+		then:
+		1 * wikitextToStardateRangeProcessorMock.process(STARDATES) >> stardateRange
+		0 * _
+		videoGameTemplate.stardateFrom == STARDATE_FROM
+		videoGameTemplate.stardateTo == STARDATE_TO
+	}
+
+	void "sets system requirements when they are found"() {
+		given:
+		Template.Part templatePart = new Template.Part(key: VideoGameTemplateParameter.REQUIREMENTS, value: SYSTEM_REQUIREMENTS)
+		Template template = new Template(parts: Lists.newArrayList(templatePart))
+		VideoGameTemplate videoGameTemplate = new VideoGameTemplate()
+
+		when:
+		videoGameTemplateContentsEnrichingProcessor.enrich(EnrichablePair.of(template, videoGameTemplate))
+
+		then:
+		0 * _
+		videoGameTemplate.systemRequirements == SYSTEM_REQUIREMENTS
+	}
+
+}
