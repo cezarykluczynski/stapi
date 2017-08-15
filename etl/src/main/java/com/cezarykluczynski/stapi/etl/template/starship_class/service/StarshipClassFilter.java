04ea040ffe38a1bf581b008a2048610f5ea9a694
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-08-15 10:24:29 +0200

    StarshipClassFilter implemented

--- etl/src/main/java/com/cezarykluczynski/stapi/etl/template/starship_class/service/StarshipClassFilter.java
+++ etl/src/main/java/com/cezarykluczynski/stapi/etl/template/starship_class/service/StarshipClassFilter.java
@@ -1,16 +1,28 @@
 package com.cezarykluczynski.stapi.etl.template.starship_class.service;
 
+import com.cezarykluczynski.stapi.etl.common.service.CategorySortingService;
 import com.cezarykluczynski.stapi.etl.template.common.service.MediaWikiPageFilter;
 import com.cezarykluczynski.stapi.sources.mediawiki.dto.Page;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+
 @Service
 public class StarshipClassFilter implements MediaWikiPageFilter {
 
+	private static final String UNNAMED_PREFIX = "Unnamed";
+
+	private final CategorySortingService categorySortingService;
+
+	@Inject
+	public StarshipClassFilter(CategorySortingService categorySortingService) {
+		this.categorySortingService = categorySortingService;
+	}
+
 	@Override
 	public boolean shouldBeFilteredOut(Page page) {
-		// TODO
-		return false;
+		return !page.getRedirectPath().isEmpty() || categorySortingService.isSortedOnTopOfAnyCategory(page)
+				|| page.getTitle().startsWith(UNNAMED_PREFIX);
 	}
 
 }
---
+++ etl/src/test/groovy/com/cezarykluczynski/stapi/etl/template/starship_class/service/StarshipClassFilterTest.groovy
@@ -0,0 +1,69 @@
+package com.cezarykluczynski.stapi.etl.template.starship_class.service
+
+import com.cezarykluczynski.stapi.etl.common.service.CategorySortingService
+import com.cezarykluczynski.stapi.sources.mediawiki.dto.Page
+import com.cezarykluczynski.stapi.sources.mediawiki.dto.PageHeader
+import com.google.common.collect.Lists
+import spock.lang.Specification
+
+class StarshipClassFilterTest extends Specification {
+
+	private CategorySortingService categorySortingServiceMock
+
+	private StarshipClassFilter starshipClassFilter
+
+	void setup() {
+		categorySortingServiceMock = Mock()
+		starshipClassFilter = new StarshipClassFilter(categorySortingServiceMock)
+	}
+
+	void "returns true when page is an effect of redirect"() {
+		given:
+		PageHeader pageHeader = Mock()
+		Page page = new Page(redirectPath: Lists.newArrayList(pageHeader))
+
+		when:
+		boolean shouldBeFilteredOut = starshipClassFilter.shouldBeFilteredOut(page)
+
+		then:
+		shouldBeFilteredOut
+	}
+
+	void "returns true when page is sorted on top of any category"() {
+		given:
+		Page page = new Page()
+
+		when:
+		boolean shouldBeFilteredOut = starshipClassFilter.shouldBeFilteredOut(page)
+
+		then:
+		1 * categorySortingServiceMock.isSortedOnTopOfAnyCategory(page) >> true
+		0 * _
+		shouldBeFilteredOut
+	}
+
+	void "returns true when page name starts with 'Unnamed '"() {
+		given:
+		Page page = new Page(title: 'Unnamed Alpha and Beta Quadrant starships (22nd century)')
+
+		when:
+		boolean shouldBeFilteredOut = starshipClassFilter.shouldBeFilteredOut(page)
+
+		then:
+		shouldBeFilteredOut
+	}
+
+	void "returns false when page does not qualify to be filtered out"() {
+		given:
+		Page page = new Page(title: '')
+
+		when:
+		boolean shouldBeFilteredOut = starshipClassFilter.shouldBeFilteredOut(page)
+
+		then:
+		1 * categorySortingServiceMock.isSortedOnTopOfAnyCategory(page) >> false
+		0 * _
+		!shouldBeFilteredOut
+	}
+
+}
