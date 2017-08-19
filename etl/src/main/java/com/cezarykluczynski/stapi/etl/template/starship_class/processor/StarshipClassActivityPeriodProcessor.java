9049196232be2a6572c0b178a7c4705f7fc53f39
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-08-19 08:13:44 +0200

    StarshipClassActivityPeriodProcessor implemented

--- etl/src/main/java/com/cezarykluczynski/stapi/etl/template/starship_class/processor/StarshipClassActivityPeriodProcessor.java
+++ etl/src/main/java/com/cezarykluczynski/stapi/etl/template/starship_class/processor/StarshipClassActivityPeriodProcessor.java
@@ -1,15 +1,81 @@
 package com.cezarykluczynski.stapi.etl.template.starship_class.processor;
 
 import com.cezarykluczynski.stapi.etl.template.starship_class.dto.ActivityPeriodDTO;
+import com.cezarykluczynski.stapi.sources.mediawiki.api.WikitextApi;
+import com.cezarykluczynski.stapi.sources.mediawiki.api.dto.PageLink;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Service
 public class StarshipClassActivityPeriodProcessor implements ItemProcessor<String, ActivityPeriodDTO> {
 
+	private static final Pattern SEPARATOR = Pattern.compile("&ndash;|-|,|onward");
+
+	private final WikitextApi wikitextApi;
+
+	@Inject
+	public StarshipClassActivityPeriodProcessor(WikitextApi wikitextApi) {
+		this.wikitextApi = wikitextApi;
+	}
+
 	@Override
 	public ActivityPeriodDTO process(String item) throws Exception {
+		List<PageLink> pageLinkList = getPeriodCandidatePageLinkList(item);
+
+		if (pageLinkList.isEmpty()) {
+			return null;
+		}
+
+		List<Integer> separatorPositions = getSeparatorPositions(item);
+
+		if (pageLinkList.size() == 1 || pageLinkList.size() == 2) {
+			return fromPageLinkListAndSeparatorsPositions(pageLinkList, separatorPositions);
+		}
+
 		return null;
+	}
+
+	private List<PageLink> getPeriodCandidatePageLinkList(String item) {
+		return wikitextApi.getPageLinksFromWikitext(item)
+				.stream()
+				.filter(this::isPeriodCandidate)
+				.collect(Collectors.toList());
+	}
+
+	private List<Integer> getSeparatorPositions(String item) {
+		List<Integer> separatorPositions = Lists.newArrayList();
+
+		Matcher matcher = SEPARATOR.matcher(item);
+		while (matcher.find()) {
+			separatorPositions.add(matcher.start());
+		}
+
+		return separatorPositions;
+	}
+
+	private ActivityPeriodDTO fromPageLinkListAndSeparatorsPositions(List<PageLink> pageLinkList, List<Integer> separatorPositions) {
+		PageLink firstPageLink = pageLinkList.get(0);
+		Integer pageLinkEndPosition = firstPageLink.getEndPosition();
+		String pageLinkTitle = firstPageLink.getTitle();
+		boolean hasOnePageLink =  pageLinkList.size() == 1;
+		String secondTitleWithSeparator = hasOnePageLink ? null : pageLinkList.get(1).getTitle();
+		String secondTitleWithoutSeparator = hasOnePageLink ? pageLinkTitle : pageLinkTitle;
+		boolean hasSeparator = separatorPositions.stream()
+				.anyMatch(separatorPosition -> separatorPosition >= pageLinkEndPosition);
+		return ActivityPeriodDTO.of(pageLinkTitle, hasSeparator ? secondTitleWithSeparator : secondTitleWithoutSeparator);
+	}
+
+	private boolean isPeriodCandidate(PageLink pageLink) {
+		return StringUtils.length(pageLink.getTitle()) > 2 && NumberUtils.isDigits(StringUtils.substring(pageLink.getTitle(), 0, 2));
 	}
 
 }
---
+++ etl/src/test/groovy/com/cezarykluczynski/stapi/etl/template/starship_class/processor/StarshipClassActivityPeriodProcessorTest.groovy
@@ -0,0 +1,49 @@
+package com.cezarykluczynski.stapi.etl.template.starship_class.processor
+
+import com.cezarykluczynski.stapi.etl.template.starship_class.dto.ActivityPeriodDTO
+import com.cezarykluczynski.stapi.sources.mediawiki.api.WikitextApiImpl
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class StarshipClassActivityPeriodProcessorTest extends Specification {
+
+	private StarshipClassActivityPeriodProcessor starshipClassActivityPeriodProcessor
+
+	void setup() {
+		starshipClassActivityPeriodProcessor = new StarshipClassActivityPeriodProcessor(new WikitextApiImpl())
+	}
+
+	@Unroll('when #text is passed, #activityPeriod is returned')
+	void "parses various text values into activityPeriod"() {
+		expect:
+		starshipClassActivityPeriodProcessor.process(text) == activityPeriod
+
+		where:
+		text                                        | activityPeriod
+		null                                        | null
+		''                                          | null
+		'No links to extract'                       | null
+		'[[2377]]'                                  | ActivityPeriodDTO.of('2377', '2377')
+		'[[2250s]]-'                                | ActivityPeriodDTO.of('2250s', null)
+		'Through [[2196]]'                          | ActivityPeriodDTO.of('2196', '2196')
+		'[[2375]]-'                                 | ActivityPeriodDTO.of('2375', null)
+		'[[29th century]]'                          | ActivityPeriodDTO.of('29th century', '29th century')
+		'[[15th century]], [[24th century]]'        | ActivityPeriodDTO.of('15th century', '24th century')
+		'Late [[24th century]]'                     | ActivityPeriodDTO.of('24th century', '24th century')
+		'mid-[[20th century]]'                      | ActivityPeriodDTO.of('20th century', '20th century')
+		'[[2280s]] &ndash; [[2370s]]'               | ActivityPeriodDTO.of('2280s', '2370s')
+		'[[2371]] ([[alternate timeline]])'         | ActivityPeriodDTO.of('2371', '2371')
+		'[[2267]]-[[2270s]]'                        | ActivityPeriodDTO.of('2267', '2270s')
+		'[[2270s]] - [[2280s]]'                     | ActivityPeriodDTO.of('2270s', '2280s')
+		'early-[[22nd century]]'                    | ActivityPeriodDTO.of('22nd century', '22nd century')
+		'[[2280s]]&ndash;[[2370s]]'                 | ActivityPeriodDTO.of('2280s', '2370s')
+		'[[2240s]] &ndash; [[2270s]]'               | ActivityPeriodDTO.of('2240s', '2270s')
+		'[[2350s]]-[[2370s]]'                       | ActivityPeriodDTO.of('2350s', '2370s')
+		'[[2374]] onward'                           | ActivityPeriodDTO.of('2374', null)
+		'[[16th century]] &ndash; [[22nd century]]' | ActivityPeriodDTO.of('16th century', '22nd century')
+		'c. [[2270s]]-[[2370s]]'                    | ActivityPeriodDTO.of('2270s', '2370s')
+		'[[2377]]&ndash;'                           | ActivityPeriodDTO.of('2377', null)
+		'[[2250s]] &ndash;'                         | ActivityPeriodDTO.of('2250s', null)
+	}
+
+}
