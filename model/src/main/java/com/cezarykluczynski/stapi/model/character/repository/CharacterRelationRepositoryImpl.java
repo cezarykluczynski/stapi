dd897dbaee00f133ac109a5507ae9b4da3b47a75
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-09-14 18:38:32 +0200

    Saving character relations

--- model/src/main/java/com/cezarykluczynski/stapi/model/character/repository/CharacterRelationRepositoryImpl.java
+++ model/src/main/java/com/cezarykluczynski/stapi/model/character/repository/CharacterRelationRepositoryImpl.java
@@ -1,20 +1,40 @@
 package com.cezarykluczynski.stapi.model.character.repository;
 
+import com.cezarykluczynski.stapi.model.character.entity.Character;
 import com.cezarykluczynski.stapi.model.character.entity.CharacterRelation;
+import com.google.common.collect.Sets;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CharacterRelationRepositoryImpl implements CharacterRelationRepositoryCustom {
 
+	private final CharacterRepository characterRepository;
+
 	@Inject
 	private CharacterRelationRepository characterRelationRepository;
 
+	@Inject
+	public CharacterRelationRepositoryImpl(CharacterRepository characterRepository) {
+		this.characterRepository = characterRepository;
+	}
+
 	@Override
 	public void linkAndSave(List<CharacterRelation> characterRelationList) {
-		// TODO
+		Set<Character> characterSet = Sets.newHashSet();
+
+		for (CharacterRelation characterRelation : characterRelationList) {
+			CharacterRelation persistedCharacterRelation = characterRelationRepository.save(characterRelation);
+			persistedCharacterRelation.getSubject().getCharacterRelations().add(persistedCharacterRelation);
+			persistedCharacterRelation.getTarget().getCharacterRelations().add(persistedCharacterRelation);
+			characterSet.add(persistedCharacterRelation.getSubject());
+			characterSet.add(persistedCharacterRelation.getTarget());
+		}
+
+		characterRepository.save(characterSet);
 	}
 
 }
---
+++ model/src/test/groovy/com/cezarykluczynski/stapi/model/character/repository/CharacterRelationRepositoryImplTest.groovy
@@ -0,0 +1,65 @@
+package com.cezarykluczynski.stapi.model.character.repository
+
+import com.cezarykluczynski.stapi.model.character.entity.Character
+import com.cezarykluczynski.stapi.model.character.entity.CharacterRelation
+import com.google.common.collect.Lists
+import com.google.common.collect.Sets
+import spock.lang.Specification
+
+class CharacterRelationRepositoryImplTest extends Specification {
+
+	private static final Long ID_1 = 11
+	private static final Long ID_2 = 12
+
+	private CharacterRepository characterRepositoryMock
+
+	private CharacterRelationRepository characterRelationRepositoryMock
+
+	private CharacterRelationRepositoryImpl characterRelationRepositoryImpl
+
+	void setup() {
+		characterRepositoryMock = Mock()
+		characterRelationRepositoryMock = Mock()
+		characterRelationRepositoryImpl = new CharacterRelationRepositoryImpl(characterRepositoryMock)
+		characterRelationRepositoryImpl.characterRelationRepository = characterRelationRepositoryMock
+	}
+
+	void "links and saves relations"() {
+		given:
+		Character subject = new Character(id: 1)
+		Character target1 = new Character(id: 2)
+		Character target2 = new Character(id: 3)
+		CharacterRelation characterRelation1 = new CharacterRelation(
+				type: 'Creator',
+				subject: subject,
+				target: target1)
+		CharacterRelation characterRelation2 = new CharacterRelation(
+				type: 'Relative',
+				subject: subject,
+				target: target2)
+
+		when:
+		characterRelationRepositoryImpl.linkAndSave(Lists.newArrayList(characterRelation1, characterRelation2))
+
+		then:
+		1 * characterRelationRepositoryMock.save(characterRelation1) >> { CharacterRelation characterRelation ->
+			characterRelation.id = ID_1
+			characterRelation
+		}
+		1 * characterRelationRepositoryMock.save(characterRelation2) >> { CharacterRelation characterRelation ->
+			characterRelation.id = ID_2
+			characterRelation
+		}
+		1 * characterRepositoryMock.save(Sets.newHashSet(subject, target1, target2))
+		0 * _
+		subject.characterRelations.contains characterRelation1
+		subject.characterRelations.contains characterRelation2
+		target1.characterRelations.contains characterRelation1
+		target2.characterRelations.contains characterRelation2
+		!target1.characterRelations.contains(characterRelation2)
+		!target2.characterRelations.contains(characterRelation1)
+		characterRelation1.id == ID_1
+		characterRelation2.id == ID_2
+	}
+
+}
