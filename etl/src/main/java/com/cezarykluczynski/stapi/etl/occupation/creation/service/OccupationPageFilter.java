a090a59684dea91828f589e68f729c18cc8038f7
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2017-10-22 12:59:39 +0200

    Filtering pages that should not be occupations

--- etl/src/main/java/com/cezarykluczynski/stapi/etl/occupation/creation/service/OccupationPageFilter.java
+++ etl/src/main/java/com/cezarykluczynski/stapi/etl/occupation/creation/service/OccupationPageFilter.java
@@ -1,25 +1,36 @@
 package com.cezarykluczynski.stapi.etl.occupation.creation.service;
 
+import com.cezarykluczynski.stapi.etl.common.processor.CategoryTitlesExtractingProcessor;
 import com.cezarykluczynski.stapi.etl.common.service.CategorySortingService;
 import com.cezarykluczynski.stapi.etl.template.common.service.MediaWikiPageFilter;
+import com.cezarykluczynski.stapi.etl.util.constant.CategoryTitle;
 import com.cezarykluczynski.stapi.sources.mediawiki.dto.Page;
+import com.google.common.collect.Sets;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.Set;
 
 @Service
 public class OccupationPageFilter implements MediaWikiPageFilter {
 
+	private static final Set<String> INVALID_TITLES = Sets.newHashSet("Commando", "Occupational hazard", "Prostitution", "Public relations");
+
 	private final CategorySortingService categorySortingService;
 
+	private final CategoryTitlesExtractingProcessor categoryTitlesExtractingProcessor;
+
 	@Inject
-	public OccupationPageFilter(CategorySortingService categorySortingService) {
+	public OccupationPageFilter(CategorySortingService categorySortingService, CategoryTitlesExtractingProcessor categoryTitlesExtractingProcessor) {
 		this.categorySortingService = categorySortingService;
+		this.categoryTitlesExtractingProcessor = categoryTitlesExtractingProcessor;
 	}
 
 	@Override
 	public boolean shouldBeFilteredOut(Page page) {
-		return !page.getRedirectPath().isEmpty() || categorySortingService.isSortedOnTopOfAnyCategory(page);
+		return !page.getRedirectPath().isEmpty() || categorySortingService.isSortedOnTopOfAnyCategory(page)
+				|| INVALID_TITLES.contains(page.getTitle())
+				|| categoryTitlesExtractingProcessor.process(page.getCategories()).contains(CategoryTitle.LISTS);
 	}
 
 }
--- etl/src/test/groovy/com/cezarykluczynski/stapi/etl/occupation/creation/service/OccupationPageFilterTest.groovy
+++ etl/src/test/groovy/com/cezarykluczynski/stapi/etl/occupation/creation/service/OccupationPageFilterTest.groovy
@@ -1,8 +1,12 @@
 package com.cezarykluczynski.stapi.etl.occupation.creation.service
 
+import com.cezarykluczynski.stapi.etl.common.processor.CategoryTitlesExtractingProcessor
 import com.cezarykluczynski.stapi.etl.common.service.CategorySortingService
+import com.cezarykluczynski.stapi.etl.util.constant.CategoryTitle
+import com.cezarykluczynski.stapi.sources.mediawiki.dto.CategoryHeader
 import com.cezarykluczynski.stapi.sources.mediawiki.dto.Page
 import com.cezarykluczynski.stapi.sources.mediawiki.dto.PageHeader
+import com.cezarykluczynski.stapi.util.tool.RandomUtil
 import com.google.common.collect.Lists
 import spock.lang.Specification
 
@@ -10,11 +14,14 @@ class OccupationPageFilterTest extends Specification {
 
 	private CategorySortingService categorySortingServiceMock
 
+	private CategoryTitlesExtractingProcessor categoryTitlesExtractingProcessorMock
+
 	private OccupationPageFilter occupationPageFilter
 
 	void setup() {
 		categorySortingServiceMock = Mock()
-		occupationPageFilter = new OccupationPageFilter(categorySortingServiceMock)
+		categoryTitlesExtractingProcessorMock = Mock()
+		occupationPageFilter = new OccupationPageFilter(categorySortingServiceMock, categoryTitlesExtractingProcessorMock)
 	}
 
 	void "returns true when redirect path is not empty"() {
@@ -43,7 +50,35 @@ class OccupationPageFilterTest extends Specification {
 		shouldBeFilteredOut
 	}
 
-	void "returns false when redirect path is empty and page is not sorted on top of any category"() {
+	void "returns true when page title is among invalid titles"() {
+		given:
+		Page page = new Page(title: RandomUtil.randomItem(OccupationPageFilter.INVALID_TITLES))
+
+		when:
+		boolean shouldBeFilteredOut = occupationPageFilter.shouldBeFilteredOut(page)
+
+		then:
+		1 * categorySortingServiceMock.isSortedOnTopOfAnyCategory(page) >> false
+		0 * _
+		shouldBeFilteredOut
+	}
+
+	void "returns true when 'Lists' category is found"() {
+		given:
+		CategoryHeader categoryHeader = Mock()
+		Page page = new Page(categories: Lists.newArrayList(categoryHeader))
+
+		when:
+		boolean shouldBeFilteredOut = occupationPageFilter.shouldBeFilteredOut(page)
+
+		then:
+		1 * categorySortingServiceMock.isSortedOnTopOfAnyCategory(page) >> false
+		1 * categoryTitlesExtractingProcessorMock.process(Lists.newArrayList(categoryHeader)) >> Lists.newArrayList(CategoryTitle.LISTS)
+		0 * _
+		shouldBeFilteredOut
+	}
+
+	void "returns false when redirect path is empty and page is not sorted on top of any category, and no 'Lists' category is found"() {
 		given:
 		Page page = new Page()
 
@@ -52,6 +87,7 @@ class OccupationPageFilterTest extends Specification {
 
 		then:
 		1 * categorySortingServiceMock.isSortedOnTopOfAnyCategory(page) >> false
+		1 * categoryTitlesExtractingProcessorMock.process(Lists.newArrayList()) >> Lists.newArrayList()
 		0 * _
 		!shouldBeFilteredOut
 	}
