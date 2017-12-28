dd487252b4315dbd10d4bd812206fb120519b95c
Cezary Kluczyński <cezary.kluczynski@decerto.pl>
2017-12-28 15:00:02 +0100

    GitHub button switched to ghbtns.com

--- server/src/main/web/src/app/app.component.ts
+++ server/src/main/web/src/app/app.component.ts
@@ -17,9 +17,9 @@ export class AppComponent {
 	}
 
 	getGitHubStar() {
-		return this.domSanitizer.bypassSecurityTrustHtml(`<a class="github-button"
-			href="https://github.com/cezarykluczynski/stapi"
-			data-size="large" data-show-count="true" aria-label="Star cezarykluczynski/stapi on GitHub">Star</a>`);
+		return this.domSanitizer.bypassSecurityTrustHtml(`
+		<iframe src="https://ghbtns.com/github-btn.html?user=cezarykluczynski&repo=stapi&type=star&count=true&size=large" frameborder="0"
+		scrolling="0" width="130px" height="32px"></iframe>`);
 	}
 
 }
