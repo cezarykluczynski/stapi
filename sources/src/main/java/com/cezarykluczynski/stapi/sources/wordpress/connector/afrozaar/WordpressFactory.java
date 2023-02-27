78475d68c64c78c973031df5d9a2c8026eeb0cce
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2023-02-27 23:47:42 +0100

    Dependency update

--- sources/src/main/java/com/cezarykluczynski/stapi/sources/wordpress/connector/afrozaar/WordpressFactory.java
+++ sources/src/main/java/com/cezarykluczynski/stapi/sources/wordpress/connector/afrozaar/WordpressFactory.java
@@ -1,15 +1,15 @@
 package com.cezarykluczynski.stapi.sources.wordpress.connector.afrozaar;
 
 import com.afrozaar.wordpress.wpapi.v2.Wordpress;
-import com.afrozaar.wordpress.wpapi.v2.util.ClientConfig;
-import com.afrozaar.wordpress.wpapi.v2.util.ClientFactory;
+import com.afrozaar.wordpress.wpapi.v2.config.ClientConfig;
+import com.afrozaar.wordpress.wpapi.v2.config.ClientFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 class WordpressFactory {
 
 	Wordpress createForUrl(String baseUrl) {
-		return ClientFactory.fromConfig(ClientConfig.of(baseUrl, null, null, false));
+		return ClientFactory.fromConfig(ClientConfig.of(baseUrl, null, null, false, false));
 	}
 
 }
